6d72a1d3b4bd2e1a11e2fb9744353e5d2d9c8863
Stefan van der Walt <stefan@sun.ac.za>
2009-02-19 12:33:32 +0200

    Add NumPy includes dir for Cython builds.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
+
+def cext(name):
+    return Extension(name, [name + ".pyx"],
+                     include_dirs=[numpy.get_include()])
 
 setup(cmdclass = {'build_ext': build_ext},
-      ext_modules = [Extension("lulu_base", ["lulu_base.pyx"]),
-                     Extension("ccomp", ["ccomp.pyx"])])
+      ext_modules = [cext('lulu_base'), cext('ccomp')])
+
