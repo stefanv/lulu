20f7b07b1bdc50d3dc7991bf60f1d99ec3a84f82
Stefan van der Walt <stefan@sun.ac.za>
2009-06-02 16:50:19 +0200

    Easy loading of image files.

--- examples/demo.py
+++ examples/demo.py
@@ -8,13 +8,17 @@ import PIL.Image
 
 import os
 
-def load_image():
+def load_image(fname=None):
     """Load example image.
 
     """
+    if fname is None:
+        fname = 'chelsea.jpg'
+
+    fname = os.path.join(os.path.dirname(__file__), 'data/' + fname)
+
     if len(sys.argv) == 2:
         img = PIL.Image.open(sys.argv[1])
     else:
-        img = PIL.Image.open(os.path.join(os.path.dirname(__file__),
-                                      'data/chelsea.jpg'))
+        img = PIL.Image.open(fname)
     return np.array(img.convert('F')).astype(int)
