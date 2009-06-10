abb00c5f545cc27f551e97c32657e6e56086dbbf
Stefan van der Walt <stefan@sun.ac.za>
2009-06-10 15:31:34 +0200

    Setup for building eggs and windows distribution.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 import numpy
 
@@ -7,8 +6,25 @@ def cext(name):
     return Extension('lulu.%s' % name, ['lulu/%s.pyx' % name],
                      include_dirs=[numpy.get_include()])
 
-setup(cmdclass = {'build_ext': build_ext},
-      ext_modules = [cext('connected_region'),
-                     cext('connected_region_handler'),
-                     cext('ccomp'),
-                     cext('base')])
+setup(name='lulu',
+      version='0.9',
+      description='Fast and efficient implementation of the 2D '
+                  'discrete pulse transform (LULU-operator)',
+      author='Stefan van der Walt',
+      author_email='stefan@sun.ac.za',
+      license='GPL',
+      url='http://dip.sun.ac.za/~stefan/code/lulu.git',
+
+      # -----
+
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[cext('connected_region'),
+                   cext('connected_region_handler'),
+                   cext('ccomp'),
+                   cext('base')],
+
+      package_data={'': ['*.txt', '*.png', '*.jpg']},
+      zip_safe=False,
+      packages=['lulu', 'lulu.tests'],
+)
+
