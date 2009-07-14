a0815732f3d098f4cc84b68b059b63f06f820580
Stefan van der Walt <stefan@sun.ac.za>
2009-07-14 16:14:13 +0200

    Clean up benchmark.

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -3,20 +3,15 @@ sys.path.insert(0, '..')
 
 from demo import load_image
 
-import numpy as np
-
-import os
 import time
-
 import lulu
-import lulu.connected_region_handler as crh
 
 img = load_image()
 
 print "Decomposing a %s matrix." % str(img.shape)
 
 tic = time.time()
-regions = lulu.decompose(img.copy())
+regions = lulu.decompose(img)
 toc = time.time()
 
 print "Execution time: %.2fs" % (toc - tic)
