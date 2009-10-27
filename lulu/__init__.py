e297a7b6bb3ff96d82a39f28a16600ae4dae6967
Stefan van der Walt <stefan@sun.ac.za>
2009-10-27 21:58:28 +0200

    Register picking for connected regions.

--- lulu/__init__.py
+++ lulu/__init__.py
@@ -1,5 +1,6 @@
 from lulu.base import *
 from lulu.connected_region import *
+import lulu.connected_region_handler
 
 import os.path as _path
 _basedir = _path.abspath(_path.join(__file__, '..'))
@@ -13,3 +14,17 @@ except:
     raise ImportError("Could not load nose.  Please install using"
                       " `easy_install nose`.")
 
+
+def _register_pickling():
+    crh = connected_region_handler
+    # Register picking functions for connected region
+    import copy_reg
+
+    def reduce_connected_region(obj):
+        return ConnectedRegion, (crh.get_shape(obj), crh.get_value(obj),
+                                 crh.get_start_row(obj), crh.get_rowptr(obj),
+                                 crh.get_colptr(obj))
+
+    copy_reg.pickle(ConnectedRegion, reduce_connected_region)
+
+_register_pickling()
